91a20a9838fbfe04a9e3f5804ef616dca958bc75
Susan Tower <Doctorsusantower@Susans-MacBook-Pro.local>
2019-08-24 16:17:00 -0500

    Working on resume POST request

--- server/app.js
+++ server/app.js
@@ -1,15 +1,43 @@
 const express = require('express');
+const axios = require('axios');
+const FormData = require('form-data');
+const meta = require('./metaData');
+
 
 // const db = require('../db/db');
 
 const app = express();
 app.use(require('morgan')('dev')); // Logs all inbound requests to console
 
-app.use(express.json());
 app.use(express.static('dist'));
 
-app.get('/api/hello', (req, res) => {
-  res.send('Hello');
-})
+
+/*
+  TEST POST
+
+  This post request:
+  1) gets json resume from client-side
+  2) posts the json to yuuvis
+  3) returns yuuvis response body
+
+*/
+app.post('/api/resume', express.json(), (req, res) => {
+  const formData = new FormData();
+  formData.append('data', meta);
+  formData.append('resume', req.body);
+  axios({
+    url: 'https://api.yuuvis.io/dms/objects',
+    headers: formData.getHeaders(),
+    data: formData,
+  })
+    .then((x) => {
+      console.log(x);
+      res.send(x);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send(e);
+    });
+});
 
 module.exports = app;
