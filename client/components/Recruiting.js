1fa2923c0642cbfeff555b7e997550751955e33a
Nicholas Miron <nicholasmiron@gmail.com>
2019-08-25 11:14:53 -0500

    Started styling recruting page

--- client/components/Recruiting.js
+++ client/components/Recruiting.js
@@ -2,16 +2,17 @@ import React from 'react';
 import RecruitingFocus from './RecruitingFocus';
 import RecruitingList from './RecruitingList';
 import RecruitingSearch from './RecruitingSearch';
+
 const axios = require('axios');
 
 class Recruiting extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { 
+    super(props);
+    this.state = {
       text: '',
-      searchProps: [], 
+      searchProps: [],
       matchingResumes: [],
-      currentResume: {}
+      currentResume: {},
     },
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,67 +22,67 @@ class Recruiting extends React.Component {
     this.handleView = this.handleView.bind(this);
   }
 
-  handleChange (e) {
+  handleChange(e) {
     // update text state
-    this.setState({text: e.target.value});
+    this.setState({ text: e.target.value });
   }
 
-  handleAddSkill (e) {
+  handleAddSkill(e) {
     e.preventDefault();
-    if(this.state.text.length === 0){
+    if (this.state.text.length === 0) {
       return;
     }
     // push text to search props
-    let newProperty = this.state.text;
-    let newSearchProps = this.state.searchProps;
+    const newProperty = this.state.text;
+    const newSearchProps = this.state.searchProps;
     newSearchProps.push(newProperty);
-    this.setState({searchProps: newSearchProps});
+    this.setState({ searchProps: newSearchProps });
 
-    document.getElementById("formtext").value = "";
-    this.setState({text: ''});
+    document.getElementById('formtext').value = '';
+    this.setState({ text: '' });
   }
 
-  handleRemoveSkill (e) {
-    let skillId = e.target.id;
-    let skillIndex = this.state.searchProps.indexOf(skillId);
-    let newSearchProps = this.state.searchProps;
+  handleRemoveSkill(e) {
+    const skillId = e.target.id;
+    const skillIndex = this.state.searchProps.indexOf(skillId);
+    const newSearchProps = this.state.searchProps;
     newSearchProps.splice(skillIndex, 1);
-    
+
     this.setState({ searchProps: newSearchProps });
   }
 
-  handleSearch () {
+  handleSearch() {
     // filter resumes using skills in searchProps
     // Add filtered resumes to matching resumes array
     // Display resumes on page
     let skills = '';
-    for(let i = 0; i < this.state.searchProps.length; i++){
-      if(i === this.state.searchProps.length - 1){
+    for (let i = 0; i < this.state.searchProps.length; i++) {
+      if (i === this.state.searchProps.length - 1) {
         skills += this.state.searchProps[i];
       } else {
-        skills += this.state.searchProps[i] + '&';
+        skills += `${this.state.searchProps[i]}&`;
       }
     }
     console.log(skills);
     axios.get(`/api/resume/${skills}`)
-    .then( (response) => {
+      .then((response) => {
       // handle success
-      console.log('success', response.data[0].basics.name);
-      // let name = response.data[0].basics.name;
-      let newMatchingResumes = response.data;
-      this.setState({matchingResumes: newMatchingResumes})
-    })
-    .catch( (error) => {
+        console.log('success', response.data[0].basics.name);
+        // let name = response.data[0].basics.name;
+        const newMatchingResumes = response.data;
+        this.setState({ matchingResumes: newMatchingResumes });
+      })
+      .catch((error) => {
       // handle error
-      console.log('error in handleSearch function:', error);
-    })
+        console.log('error in handleSearch function:', error);
+      });
   }
 
-  handleView (resume) {
-    this.setState({currentResume: resume});
+  handleView(resume) {
+    this.setState({ currentResume: resume });
   }
 
-  render () {
+  render() {
     return (
     <div className='Recruiter'>
       <h1 id="navbar">Recruiter Dashboard</h1>
@@ -91,10 +92,10 @@ class Recruiting extends React.Component {
                         handleRemoveSkill={this.handleRemoveSkill}
                         searchProps={this.state.searchProps}
                         handleSearch={this.handleSearch}/>
-      <div className='recruiter-main-container'>
+      {/* <div className='recruiter-main-container'> */}
         <RecruitingList className='recruiter-sidebar' resumes={this.state.matchingResumes} handleView={this.handleView}/>
         <RecruitingFocus className='recruiter-focus' focusResume={this.state.currentResume}/>
-      </div>
+      {/* </div> */}
     </div>
     );
   }
--- client/components/RecruitingFocus.js
+++ client/components/RecruitingFocus.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const RecruitingFocus = ({ focusResume }) => {
-  const { basics, education, skills, work } = focusResume;
+  const {
+    basics, education, skills, work,
+  } = focusResume;
   return (!Object.keys(focusResume).length) ? (
     null
   ) : (
-  <div>
+  <div className={'selectedResume'}>
     <div>
       <div>{basics.name}</div>
       <div>{basics.label}</div>
@@ -16,7 +19,11 @@ const RecruitingFocus = ({ focusResume }) => {
       <div>{work[0].position}</div>
       <div>{work[0].highlights[0]}</div>
     </div>
-  </div>)
-}
+  </div>);
+};
+
+RecruitingFocus.propTypes = {
+  focusResume: PropTypes.func,
+};
 
 export default RecruitingFocus;
