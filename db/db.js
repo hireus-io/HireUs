252bd19a8391bce0adc0950b7069b56b45a7f417
Nicholas Miron <nicholasmiron@gmail.com>
2019-08-24 22:24:02 -0500

    Got it to download a pdf yay

--- db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const { Schema } = mongoose;
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/<FILL_ME_IN>',);
-
-module.exports = {};
--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cp = require('child_process');
+const util = require('util');
+const fs = require('fs');
 
-// const db = require('../db/db');
+const exec = util.promisify(cp.exec);
 
 const app = express();
 app.use(require('morgan')('dev')); // Logs all inbound requests to console
@@ -8,8 +11,20 @@ app.use(require('morgan')('dev')); // Logs all inbound requests to console
 app.use(express.json());
 app.use(express.static('dist'));
 
-app.get('/api/hello', (req, res) => {
-  res.send('Hello');
-})
+app.post('/api/resume', (req, res) => {
+  fs.writeFile('resume.json', JSON.stringify(req.body.resume, null, 2), () => {
+    exec('resume export resume.pdf')
+      .then(() => {
+        res.download('resume.pdf', 'resume.pdf');
+      })
+      .catch(() => {
+        res.end();
+      });
+  });
+});
+
+app.get('/api/resume', (req, res) => {
+  res.download('resume.pdf', 'resume.pdf');
+});
 
 module.exports = app;
